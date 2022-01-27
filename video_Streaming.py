188b06232479ea2ea0cc668c424b95aed0fe76cb
JEGADESH312 <58292233+JEGADESH312@users.noreply.github.com>
2022-01-27 17:51:24 +0530

    Compression  Added

--- video_Streaming.py
+++ video_Streaming.py
@@ -1,38 +1,22 @@
 import cv2
+
 class Video(object):
     def __init__(self):
         self.video=cv2.VideoCapture(0)
+        self.count=0
     def __del__(self):
         self.video.release()
     def get_frame(self):
-        ret,frame=self.video.read()
-        ret,jpg=cv2.imencode('.jpg',frame)
+        ret,frame=self.video.read() 
+        ret,jpg=cv2.imencode('.jpg',frame,Compress(50))
+        self.count +=1
+        
         return jpg.tobytes()
 
 
 
 
 
+def Compress(value):
+    return [int(cv2.IMWRITE_JPEG_QUALITY), value]
 
-
-
-
-# import fractions
-# import cv2
-
-# video=cv2.VideoCapture(0)
-
-# faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# print(faceDetect)
-# while True:
-#     ret,frame=video.read()
-#     print(ret)
-#     positions=faceDetect.detectMultiScale(frame,1.3,5)
-#     for x,y,w,h in positions:
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,255),2)
-#     cv2.imshow('Frame',frame)
-#     k=cv2.waitKey(1)
-#     if k==ord('q'):
-#         break
-# video.release()
-# cv2.destroyAllWindows()
